95bcd5895d34587c1acde88451f7fd9705d7bb6f
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2015-11-17 22:00:25 -0600

    Again...

---
+++ DildenFeedback.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace dilden\feedbackwidget;
+
+/**
+ * This is just an example.
+ */
+class dil-feedback extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Testing the update!";
+    }
+}
