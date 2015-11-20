c1c0f3fc810ac461dd1b27474c513bf07a7470e9
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2015-11-20 15:52:34 -0600

    figured it out now

--- DildenFeedbackAsset.php
+++ DildenFeedbackAsset.php
@@ -6,36 +6,19 @@ use yii\web\AssetBundle;
 
 class DildenFeedbackAsset extends AssetBundle {
 
-    // public $sourcePath = '@app/components/yii2feedbackwidget/assets';
-
-    // public $css = ['@app/components/yii2feedbackwidget/assets/feedback.min.css'];
-    // public $js = ['@app/components/yii2feedbackwidget/assets/feedback.min.js'];
-
-    // public $depends = [
-    // ];
-
 	public function init()
     {
         $this->sourcePath = '@app/components/yii2feedbackwidget/assets/';
-        $this->basePath = '@webroot';
-        $this->baseUrl = '@web';
         $this->css = [ 
-		    'assets/feedback.min.css'
+		    'feedback.min.css'
 		]; 
 		$this->js = [
-            'assets/feedback.min.js'
+            'feedback.min.js'
 		];
         $this->depends = [
             'yii\web\JqueryAsset',
         ];
         parent::init();
     }
-
- //    protected function setSourcePath($path)
- //    {
- //        if (empty($this->sourcePath)) {
- //            $this->sourcePath = $path;
- //        }
- //    }
 
 }
